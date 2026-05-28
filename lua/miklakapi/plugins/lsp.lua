ffa1f70f25aa200dd4f9886e6dde564c5841cb6e
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-05-28 18:28:57 +0200

    ts-lit-plugin

--- lua/miklakapi/plugins/lsp.lua
+++ lua/miklakapi/plugins/lsp.lua
@@ -1,6 +1,22 @@
 local vue_language_server_path = vim.fn.stdpath("data") ..
     "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
+local function get_node_module_path(module_name)
+    local current_dir = vim.fn.getcwd()
+
+    while current_dir ~= "/" do
+        local module_path = current_dir .. "/node_modules/" .. module_name
+
+        if vim.fn.isdirectory(module_path) == 1 then
+            return module_path
+        end
+
+        current_dir = vim.fn.fnamemodify(current_dir, ":h")
+    end
+
+    return nil
+end
+
 local servers = {
     "lua_ls",
     "vtsls",
@@ -42,6 +58,32 @@ return {
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            local ts_lit_plugin_path = get_node_module_path("ts-lit-plugin")
+
+            local vtsls_global_plugins = {
+                {
+                    name = "@vue/typescript-plugin",
+                    location = vue_language_server_path,
+                    languages = {
+                        "vue",
+                    },
+                    configNamespace = "typescript",
+                },
+            }
+
+            if ts_lit_plugin_path ~= nil then
+                table.insert(vtsls_global_plugins, {
+                    name = "ts-lit-plugin",
+                    location = ts_lit_plugin_path,
+                    languages = {
+                        "javascript",
+                        "javascriptreact",
+                        "typescript",
+                        "typescriptreact",
+                    },
+                })
+            end
+
             vim.lsp.config("lua_ls", {
                 capabilities = capabilities,
                 settings = {
@@ -68,18 +110,20 @@ return {
                     "vue",
                 },
                 settings = {
+                    typescript = {
+                        tsdk = "./node_modules/typescript/lib",
+                        preferences = {
+                            includePackageJsonAutoImports = "auto",
+                        },
+                    },
+                    javascript = {
+                        preferences = {
+                            includePackageJsonAutoImports = "auto",
+                        },
+                    },
                     vtsls = {
                         tsserver = {
-                            globalPlugins = {
-                                {
-                                    name = "@vue/typescript-plugin",
-                                    location = vue_language_server_path,
-                                    languages = {
-                                        "vue",
-                                    },
-                                    configNamespace = "typescript",
-                                },
-                            },
+                            globalPlugins = vtsls_global_plugins,
                         },
                     },
                 },
