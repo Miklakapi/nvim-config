a4edb4dd549ac2aaa65ef3e85f0f68543f59ede6
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-05-31 16:49:39 +0200

    lualine

---
+++ lua/miklakapi/plugins/lualine.lua
@@ -0,0 +1,76 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    event = "VeryLazy",
+    config = function()
+        local lualine = require("lualine")
+
+        lualine.setup({
+            options = {
+                theme = "auto",
+                icons_enabled = true,
+
+                component_separators = {
+                    left = "",
+                    right = "",
+                },
+
+                section_separators = {
+                    left = "",
+                    right = " ",
+                },
+
+                globalstatus = true,
+            },
+
+            sections = {
+                lualine_a = {
+                    "mode",
+                },
+
+                lualine_b = {
+                    "branch",
+                    "diff",
+                },
+
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+
+                lualine_x = {
+                    "diagnostics",
+                    "filetype",
+                },
+
+                lualine_y = {
+                    "progress",
+                },
+
+                lualine_z = {
+                    "location",
+                },
+            },
+
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+                lualine_x = {
+                    "location",
+                },
+                lualine_y = {},
+                lualine_z = {},
+            },
+        })
+    end,
+}
