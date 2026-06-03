eeefcba30895920f17b92f503129f304dbf50fd3
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-03 21:02:25 +0200

    lualine style

--- lua/miklakapi/plugins/lualine.lua
+++ lua/miklakapi/plugins/lualine.lua
@@ -7,9 +7,54 @@ return {
     config = function()
         local lualine = require("lualine")
 
+        local colors = {
+            bg = "#11111b",
+            fg = "#cdd6f4",
+            blue = "#89b4fa",
+            green = "#a6e3a1",
+            purple = "#cba6f7",
+            red = "#f38ba8",
+            yellow = "#f9e2af",
+            git = "#313244",
+            file = "#181825",
+        }
+
+        local theme = {
+            normal = {
+                a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
+                b = { fg = colors.fg, bg = colors.git },
+                c = { fg = colors.fg, bg = colors.file },
+            },
+            insert = {
+                a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+                b = { fg = colors.fg, bg = colors.git },
+                c = { fg = colors.fg, bg = colors.file },
+            },
+            visual = {
+                a = { fg = colors.bg, bg = colors.purple, gui = "bold" },
+                b = { fg = colors.fg, bg = colors.git },
+                c = { fg = colors.fg, bg = colors.file },
+            },
+            replace = {
+                a = { fg = colors.bg, bg = colors.red, gui = "bold" },
+                b = { fg = colors.fg, bg = colors.git },
+                c = { fg = colors.fg, bg = colors.file },
+            },
+            command = {
+                a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
+                b = { fg = colors.fg, bg = colors.git },
+                c = { fg = colors.fg, bg = colors.file },
+            },
+            inactive = {
+                a = { fg = colors.fg, bg = colors.bg },
+                b = { fg = colors.fg, bg = colors.bg },
+                c = { fg = colors.fg, bg = colors.bg },
+            },
+        }
+
         lualine.setup({
             options = {
-                theme = "auto",
+                theme = theme,
                 icons_enabled = true,
 
                 component_separators = {
@@ -18,7 +63,7 @@ return {
                 },
 
                 section_separators = {
-                    left = "",
+                    left = " ",
                     right = " ",
                 },
 
